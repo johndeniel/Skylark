c8239e92e71921516087641f880b47a7005b1457
John Deniel <johndeniel.d37@gmail.com>
2023-09-05 11:40:07 +0000

    fix:settings

--- app/(user)/dashboard/settings/page.tsx
+++ app/(user)/dashboard/settings/page.tsx
@@ -24,19 +24,23 @@ export const metadata = {
 
 export default async function SettingsPage() {
   return (
+
     <Shell className="gap-4">
       <Header
         title="Settings"
         description="Manage account and website settings."
         size="sm"
       />
+
       <Separator />
-      <Tabs defaultValue="account" className="hidden w-full md:block">
-        <TabsList className="grid w-full grid-cols-3">
+
+      <Tabs defaultValue="appearance" className="hidden w-full md:block">
+
+        <TabsList className="grid w-full grid-cols-2">
           <TabsTrigger value="account">Account</TabsTrigger>
           <TabsTrigger value="appearance">Appearance</TabsTrigger>
-          <TabsTrigger value="reminder">Reminder</TabsTrigger>
         </TabsList>
+
         <TabsContent value="account">
           <Card>
             <CardHeader>
@@ -47,27 +51,12 @@ export default async function SettingsPage() {
               </CardDescription>
             </CardHeader>
             <CardContent className="space-y-10">
-        
-              <div>
-                <p className="font-semibold">Profile</p>
-                <div className="space-y-8">
-                  <p className="text-muted-foreground">
-                    Manage your profile information with clerk
-                  </p>
-                  <Link
-                    href="/journal/settings/user-profile"
-                    className={cn(buttonVariants())}
-                  >
-                    Manage Profile
-                  </Link>
-                </div>
-              </div>
+              <h1>Not Implemented Yet!</h1>
             </CardContent>
           </Card>
         </TabsContent>
 
-
-         <TabsContent value="appearance">
+        <TabsContent value="appearance">
           <Card>
             <CardHeader>
               <CardTitle>Appearance</CardTitle>
@@ -82,80 +71,22 @@ export default async function SettingsPage() {
           </Card>
         </TabsContent>
 
+      </Tabs>
 
-        <TabsContent value="reminder">
-          <Card>
+      <div className="space-y-10 md:hidden">
+        <Card>
             <CardHeader>
-              <CardTitle>Reminder</CardTitle>
+              <CardTitle>Appearance</CardTitle>
               <CardDescription>
-                Customize the reminder frequency you are comfortable with.
+                Customize the appearance of the app. Automatically switch between
+                day and night themes.
               </CardDescription>
             </CardHeader>
             <CardContent className="space-y-10">
-          
+              <AppearanceForm />
             </CardContent>
-            <CardFooter>
-              We&apos;ll send you a reminder email every day/weekly at 9:00 Pm
-              UTC
-            </CardFooter>
           </Card>
-        </TabsContent>
-      </Tabs>
-      <div className="space-y-10 md:hidden">
-        <Card>
-          <CardHeader>
-            <CardTitle>Account</CardTitle>
-            <CardDescription>
-              Make changes to your account here. Click save when you&apos;re
-              done.
-            </CardDescription>
-          </CardHeader>
-          <CardContent className="space-y-10">
-           
-            <div>
-              <p className="font-semibold">Profile</p>
-              <div className="space-y-8">
-                <p className="text-muted-foreground">
-                  Manage your profile information with clerk
-                </p>
-                <Link
-                  href="/journal/settings/user-profile"
-                  className={cn(buttonVariants())}
-                >
-                  Manage Profile
-                </Link>
-              </div>
-            </div>
-          </CardContent>
-        </Card>
-        <Card>
-          <CardHeader>
-            <CardTitle>Appearance</CardTitle>
-            <CardDescription>
-              Customize the appearance of the app. Automatically switch between
-              day and night themes.
-            </CardDescription>
-          </CardHeader>
-          <CardContent className="space-y-10">
-           
-          </CardContent>
-        </Card>
-        <Card>
-          <CardHeader>
-            <CardTitle>Reminder</CardTitle>
-            <CardDescription>
-              Customize the reminder frequency you are comfortable with.
-            </CardDescription>
-          </CardHeader>
-          <CardContent className="space-y-10">
-         
-          
-          </CardContent>
-          <CardFooter>
-            We&apos;ll send you a reminder email every day/weekly at 9:00 Pm UTC
-          </CardFooter>
-        </Card>
-      </div>
+        </div>
     </Shell>
   )
 }
