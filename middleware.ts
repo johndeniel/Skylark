9e42c6b020af52922268d998d9aac2a9cec7e7b5
John Deniel <johndeniel.d37@gmail.com>
2023-11-02 00:43:11 +0000

    impl:monorepo

--- middleware.ts
+++
@@ -1,9 +0,0 @@
-import { authMiddleware } from '@clerk/nextjs';
-
-export default authMiddleware({
-  publicRoutes: ["/","/dashboard"]
-});
-
-export const config = {
-  matcher: ['/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)'],
-};
