d6d2e243c176e7754e61037dc9182f5126cb0cf4
johndeniel <johndeniel.d37@gmail.com>
2023-11-18 21:27:26 +0800

    update:guide

--- apps/docs/next.config.js
+++ apps/docs/next.config.js
@@ -1,16 +1,6 @@
-const withNextra = require("nextra")({
-  theme: "nextra-theme-docs",
-  themeConfig: "./theme.config.tsx",
-});
+const withNextra = require('nextra')({
+  theme: 'nextra-theme-docs',
+  themeConfig: './theme.config.tsx',
+})
 
-module.exports = withNextra({
-  async redirects() {
-    return [
-      {
-        source: "/docs",
-        destination: "/docs/introduction",
-        permanent: false,
-      },
-    ];
-  },
-});
+module.exports = withNextra()
