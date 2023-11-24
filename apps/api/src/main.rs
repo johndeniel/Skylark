0fdacd907086ff5b65d4663cd2667b3fc23baf06
John Deniel <johndeniel.d37@gmail.com>
2023-11-24 11:01:05 +0000

    laravel

--- apps/api/src/main.rs
+++ apps/api/src/main.rs
@@ -1,8 +1,4 @@
 use skylark::run;
 
 #[tokio::main]
-async fn main() {
-    // Call the `run` function, which executes the logic defined in the `run` function
-    // The `await` keyword suspends the execution of the `main` function until `run` completes
-    run().await;
-}
+async fn main() { run().await; }
